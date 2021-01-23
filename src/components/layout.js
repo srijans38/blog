95b36415ebb54e3672b2c2890c4e774fa5a73ac8
Srijan Sharma <srijans38@gmail.com>
2021-01-24 01:05:45 +0530

    add footer and fix mobile font size

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 export default function Layout({ mt, children }) {
   return (
-    <div
-      className={`container ${
-        mt ?? 'mt-12'
-      } px-6 max-w-5xl sm:mx-auto font-body box-border`}
-    >
-      {children}
-    </div>
+    <>
+      <div
+        className={`container ${
+          mt ?? 'mt-12'
+        } mb-24 px-6 max-w-5xl sm:mx-auto font-body box-border`}
+      >
+        {children}
+      </div>
+      <footer className="flex w-full h-16 items-center justify-center px-6 border-t">
+        <p>
+          Made with <span className="text-red-500">♥</span> and{' '}
+          <span>Gatsby</span>
+        </p>
+      </footer>
+    </>
   );
 }
